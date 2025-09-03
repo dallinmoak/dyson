61e7f7af9e89390438351c3faddf374d5879d170
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-09-03 12:58:52 -0600

    embed parent id into each recipe and a flag for if it's an alt recipe

--- src/app/materials/[name]/page.js
+++ src/app/materials/[name]/page.js
@@ -4,9 +4,25 @@ import Material from "@/lib/data";
 export default async function Page({ params }) {
   const { name } = await params;
   const material = await Material.getByName(name);
+  let materialData = {
+    ...material,
+  }
+  if (material.recipe) {
+    materialData.recipe = {
+      ...material.recipe,
+      materialId: material._id.toString(),
+    }
+  }
+  if (material.altRecipes) {
+    materialData.altRecipes = material.altRecipes.map((recipe) => ({
+      ...recipe,
+      isAlt: true,
+      materialId: material._id.toString(),
+    }));
+  }
   return (
     <>
-      <MaterialComponent material={material} />
+      <MaterialComponent material={materialData} />
     </>
   );
 }
